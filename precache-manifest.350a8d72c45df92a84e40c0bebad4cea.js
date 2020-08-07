1f729e05ebcf9c7ce56946b98a7bba317aeb11a9
gatemediang <estolog@gmail.com>
2020-08-07 06:28:48 +0100

    Updates

---
+++ precache-manifest.350a8d72c45df92a84e40c0bebad4cea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ce2eeff11f2da7920222be8e0a9601",
+    "url": "/NiFix-NoteApp/index.html"
+  },
+  {
+    "revision": "9535696d4fbb5e95b5af",
+    "url": "/NiFix-NoteApp/static/css/main.e5d4db96.chunk.css"
+  },
+  {
+    "revision": "a88d723be051ca90a63a",
+    "url": "/NiFix-NoteApp/static/js/2.91960fe2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NiFix-NoteApp/static/js/2.91960fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9535696d4fbb5e95b5af",
+    "url": "/NiFix-NoteApp/static/js/main.1f8b5958.chunk.js"
+  },
+  {
+    "revision": "2de6b189bf7fe7f334e5",
+    "url": "/NiFix-NoteApp/static/js/runtime-main.85b3d359.js"
+  }
+]);
